317a381f1e0780b5da33bbf0992d99b0cc05de2a
Talent Branjerdporn <talentbran@hotmail.com>
2017-07-06 19:23:22 +1000

    Add project files.

---
+++ TalentBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TalentBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
